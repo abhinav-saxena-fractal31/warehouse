6cb2707b726459b42a6284e581e28379a614fad7
abhinav-saxena-fractal31 <abhinav@fractal31.com>
2023-07-05 10:43:53 +0000

    Refactor `upgrade` to `upgrade`, `lock_tables`, `add_last_serial_column`, `update_last_serial_values`, `alter_last_serial_column`, `create_trigger_function`, `create_trigger`

--- warehouse/migrations/versions/104b4c56862b_denormalize_serial_onto_project.py
+++ warehouse/migrations/versions/104b4c56862b_denormalize_serial_onto_project.py
@@ -23,14 +23,30 @@ from alembic import op
 
 revision = "104b4c56862b"
 down_revision = "fb3278418206"
-
-
 def upgrade():
-    # Make sure that no new serials can be added while we're working on these
-    # tables.
+    """Upgrade the database schema.
+
+    This function performs the necessary database schema upgrades.
+    It adds a new column 'last_serial' to the 'packages' table and updates its values based on the 'journals' table.
+    It also sets up a trigger function that ensures 'last_serial' is automatically updated on INSERT/UPDATE/DELETE in the 'journals' table.
+    """
+
+    lock_tables()
+    add_last_serial_column()
+    update_last_serial_values()
+    alter_last_serial_column()
+    create_trigger_function()
+    create_trigger()
+
+
+def lock_tables():
+    """Lock the 'packages' and 'journals' tables."""
     op.execute("LOCK TABLE packages IN EXCLUSIVE MODE")
     op.execute("LOCK TABLE journals IN EXCLUSIVE MODE")
 
+
+def add_last_serial_column():
+    """Add 'last_serial' column to the 'packages' table."""
     op.add_column(
         "packages",
         sa.Column(
@@ -38,59 +54,66 @@ def upgrade():
         ),
     )
 
-    # Set packages.last_serial for each package to the current maximum serial
-    # as computed by the journals table.
+
+def update_last_serial_values():
+    """Update the 'last_serial' column for each package in the 'packages' table."""
     op.execute(
-        """ UPDATE packages
-            SET last_serial = j.last_serial
-            FROM (
-                SELECT name,
-                       max(id) as last_serial
-                FROM journals
-                GROUP BY name
-            ) as j
-            WHERE j.name = packages.name
+        """UPDATE packages
+           SET last_serial = j.last_serial
+           FROM (
+               SELECT name,
+                      max(id) as last_serial
+               FROM journals
+               GROUP BY name
+           ) as j
+           WHERE j.name = packages.name
         """
     )
 
-    # Now that data has been backfilled, we'll set nullable to False.
+
+def alter_last_serial_column():
+    """Alter the 'last_serial' column in the 'packages' table to be non-nullable."""
     op.alter_column("packages", "last_serial", nullable=False)
 
-    # Setup a trigger function that will ensure that on INSERT/UPDATE/DELETE we
-    # populate the packages.last_serial attribute with the new serial number.
-    # 113359
+
+def create_trigger_function():
+    """Create a trigger function to maintain the 'last_serial' attribute in the 'packages' table."""
     op.execute(
-        """ CREATE OR REPLACE FUNCTION maintain_project_last_serial()
-            RETURNS TRIGGER AS $$
-            DECLARE
-                targeted_name text;
-            BEGIN
-                IF TG_OP = 'INSERT' THEN
-                    targeted_name := NEW.name;
-                ELSEIF TG_OP = 'UPDATE' THEN
-                    targeted_name := NEW.name;
-                ELSIF TG_OP = 'DELETE' THEN
-                    targeted_name := OLD.name;
-                END IF;
+        """CREATE OR REPLACE FUNCTION maintain_project_last_serial()
+           RETURNS TRIGGER AS $$
+           DECLARE
+               targeted_name text;
+           BEGIN
+               IF TG_OP = 'INSERT' THEN
+                   targeted_name := NEW.name;
+               ELSEIF TG_OP = 'UPDATE' THEN
+                   targeted_name := NEW.name;
+               ELSIF TG_OP = 'DELETE' THEN
+                   targeted_name := OLD.name;
+               END IF;
 
-                UPDATE packages
-                SET last_serial = j.last_serial
-                FROM (
-                    SELECT max(id) as last_serial
-                    FROM journals
-                    WHERE journals.name = targeted_name
-                ) as j
-                WHERE packages.name = targeted_name;
+               UPDATE packages
+               SET last_serial = j.last_serial
+               FROM (
+                   SELECT max(id) as last_serial
+                   FROM journals
+                   WHERE journals.name = targeted_name
+               ) as j
+               WHERE packages.name = targeted_name;
 
-                RETURN NULL;
-            END;
-            $$ LANGUAGE plpgsql;
+               RETURN NULL;
+           END;
+           $$ LANGUAGE plpgsql;
         """
     )
+
+
+def create_trigger():
+    """Create a trigger to update the 'last_serial' attribute in the 'packages' table."""
     op.execute(
-        """ CREATE TRIGGER update_project_last_serial
-            AFTER INSERT OR UPDATE OR DELETE ON journals
-            FOR EACH ROW EXECUTE PROCEDURE maintain_project_last_serial();
+        """CREATE TRIGGER update_project_last_serial
+           AFTER INSERT OR UPDATE OR DELETE ON journals
+           FOR EACH ROW EXECUTE PROCEDURE maintain_project_last_serial();
         """
     )
 
